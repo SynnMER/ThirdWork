f3018b0909f6ffaf3a5496596663a8b67074c999
Artur Ilyasov <Scorpion_run_142@mail.ru>
2024-03-19 11:01:08 +0500

    Добавьте файлы проекта.

---
+++ ConcreteCreator/ConcreteCreator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Creator;
+namespace ConcreteCreator
+{
+    class Diamond : Parallelogram
+    {
+    }
+    class Parallelogram : Quadrangle
+    {
+    }
+    class Quadrangle : IFigure
+    {
+    }
+    class Square : Diamond
+    {
+    }
+    public class DiamondCreator : ICreator
+    {
+        public IFigure FactoryMethod()
+        {
+            return new Diamond();
+        }
+    }
+    public class ParallelogramCreator : ICreator
+    {
+        public IFigure FactoryMethod()
+        {
+            return new Parallelogram();
+        }
+    }
+    public class QuadrangleCreator : ICreator
+    {
+        public IFigure FactoryMethod()
+        {
+            return new Quadrangle();
+        }
+    }
+    public class SquareCreator : ICreator
+    {
+        public IFigure FactoryMethod()
+        {
+            return new Square();
+        }
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConcreteCreator;
+using Creator;
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreator squareCreator = new SquareCreator();
+            ICreator parallelogramCreator = new ParallelogramCreator();
+            ICreator diamondCreator = new DiamondCreator();
+            ICreator quadrangleCreator = new QuadrangleCreator();
+
+
+            IFigure quadrangle = quadrangleCreator.FactoryMethod();
+            IFigure parallelogram = parallelogramCreator.FactoryMethod();
+            IFigure diamond = diamondCreator.FactoryMethod();
+            IFigure square = squareCreator.FactoryMethod();
+        }
+
+    }
+}
